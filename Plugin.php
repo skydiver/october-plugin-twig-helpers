e529352b04ffde180b55f2e0111eaa607e438d6c
Martin <skydiver@users.noreply.github.com>
2017-08-26 18:53:21 -0300

    * code linting * new CSS functions

--- Plugin.php
+++ Plugin.php
@@ -1,36 +1,37 @@
 <?php
 
-    namespace Martin\Helpers;
+namespace Martin\Helpers;
 
-    use Backend;
-    use System\Classes\PluginBase;
+use Backend;
+use System\Classes\PluginBase;
 
-    class Plugin extends PluginBase {
+class Plugin extends PluginBase {
 
-        public function pluginDetails() {
-            return [
-                'name'        => 'Twig Helpers',
-                'description' => 'Twig Helpers to make life easier',
-                'author'      => 'Martin M.',
-                'icon'        => 'icon-life-ring',
-                'homepage'    => 'https://github.com/skydiver/'
-            ];
-        }
-
-        public function registerMarkupTags() {
-            return [
-                'filters' => [
-                    'gravatar' => ['Martin\Helpers\Classes\Gravatar', 'filterGravatar'],
-                    'pr'       => ['Martin\Helpers\Classes\PrintR'  , 'pr'],
-                    'prd'      => ['Martin\Helpers\Classes\PrintR'  , 'prd'],
-
-                ],
-                'functions' => [
-                    'isActive' => ['Martin\Helpers\Classes\CSS', 'isActive'],
-                ]
-            ];
-        }
-
+    public function pluginDetails() {
+        return [
+            'name'        => 'Twig Helpers',
+            'description' => 'Twig Helpers to make life easier',
+            'author'      => 'Martin M.',
+            'icon'        => 'icon-life-ring',
+            'homepage'    => 'https://github.com/skydiver/'
+        ];
     }
+
+    public function registerMarkupTags() {
+        return [
+            'filters' => [
+                'gravatar' => ['Martin\Helpers\Classes\Gravatar', 'filterGravatar'],
+                'pr'       => ['Martin\Helpers\Classes\PrintR'  , 'pr'],
+                'prd'      => ['Martin\Helpers\Classes\PrintR'  , 'prd'],
+
+            ],
+            'functions' => [
+                'isActive'      => ['Martin\Helpers\Classes\CSS', 'isActive'],
+                'isActiveArray' => ['Martin\Helpers\Classes\CSS', 'isActiveArray'],
+            ]
+        ];
+    }
+
+}
 
 ?>
--- classes/CSS.php
+++ classes/CSS.php
@@ -1,15 +1,57 @@
 <?php
 
-    namespace Martin\Helpers\Classes;
+namespace Martin\Helpers\Classes;
 
-    class CSS {
+class CSS {
 
-        public static function isActive($current, $expected, $class='active') {
-            if($current->baseFileName == $expected) {
-                return ' class="' . $class . '"';
+    /**
+     * Check for current page and return CSS active class
+     *
+     * @param object $current  Current page object
+     * @param string $expected Expected string (support wildcards)
+     * @param bool   $attrib   Return class attribute
+     * @param string $class    CSS class to apply
+     *
+     * @return string active class (and attribute)
+     */
+    public static function isActive($current, $expected, $attrib=true, $class='active') {
+        if (str_is($expected, $current->baseFileName)) {
+            return ($attrib) ? self::_addClassAttrib($class) : $class;
+        }
+    }
+
+    /**
+     * Check for current page and return CSS active class
+     * from an array of expected strings
+     *
+     * @param object $current       Current page object
+     * @param array  $expectedArray Array of expected strings (support wildcards)
+     * @param bool   $attrib        Return class attribute
+     * @param string $class         CSS class to apply
+     *
+     * @return string active class (and attribute)
+     */
+    public static function isActiveArray($current, $expectedArray, $attrib=true, $class='active') {
+        $arr = explode('|', $expectedArray);
+        foreach ($arr as $expected) {
+            $is = self::isActive($current, $expected, $attrib, $class);
+            if ($is) {
+                return $is;
             }
         }
-
     }
+
+    /**
+     * Return class attribute with supplied class
+     *
+     * @param string $class CSS class(es)
+     *
+     * @return string CSS class(es) with HTML attribute
+     */
+    private static function _addClassAttrib($class) {
+        return ' class="' . $class . '"';
+    }
+
+}
 
 ?>
